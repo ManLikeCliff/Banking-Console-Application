b13724fdf7ed0c6f7507f640fad07da6cacbdfbe
support <support@DESKTOP-CRUHND7>
2021-11-03 08:13:05 +0100

    Add project files.

---
+++ Banking Console Application/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Banking_Console_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount( money: 100, name: "Cliff");
+
+            bankAccount.AddMoney(amount: 50);
+
+            bankAccount.getInfo();
+
+            bankAccount.Subtract(Money: 100);
+
+            bankAccount.getInfo();
+        }
+    }
+}
